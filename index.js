64202c9c18fb2a76302e4ef3ced8f88c109934a1
Harshith M <mharshith200@gmail.com>
2026-05-05 18:45:45 +0530

    add github actions

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello World!");
+console.log("I'm learning DevOps and CI/CD pipelines.");
+console.log("This is my first commit to the repository.");
+console.log("I'm using GitHub Actions to automate my workflow.");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Hello World!");
+setTimeout(() => {
+    console.log("This message is displayed after a delay.");
+}, 3000);
+console.log("This message is displayed immediately.");
